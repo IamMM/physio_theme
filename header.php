01b2536cff13d048331f05ec596d2a49550bfadb
Maximilian Maske <maximilian.maske@gmail.com>
2017-07-04 13:38:17 +0200

    icons mobile

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
   <body>
       <div class='container blog-header'>
             <div class="row">
-              <div class="col-md-3">
+              <div class="col-sm-3">
                 <div id="logo">
                   <a href="/" class="home-link">
                     <h1 class="blog-title"><?php bloginfo('name')?></h1>
@@ -42,40 +42,41 @@
                   </a>
                 </div>
               </div>
-              <div class="col-md-8 col-md-offset-1 ">
+              <div class="col-sm-8 col-sm-offset-1 ">
                 <div class="row hidden-xs">
                   <div class="hover-info">
                     <div> <p>030 / 76 76 488 - 2 <small> Bei Fragen sind wir für Sie da.</small></p></div>
                     <div> <p>Karl-Marx-Straße 188, 12043 Berlin</p></div>
                     <div> <p>Mo-Fr 8:00 - 21:00 Uhr | Sa 8:00 - 18:00 Uhr</p></div>
                     <div> <p>kontakt@wanja-richter.de</p></div>
-                    <div> <p>+49 0157 / 345 6789 <small> Terminbuchen über den WhatsApp Chat</p></small></div>
+                    <div> <p>+49 0157 / 345 6789 <small> Terminbuchen über den WhatsApp Chat</small></p></div>
                   </div>
                 </div>
                 <div class="row">
-                  <ul id="icons" class="list-inline">
-                    <li>
+                  <ul class="list-inline">
+                    <li class="icon">
                       <a href="tel:03076764882">
-                        <img src="<?php bloginfo('template_url')?>/icons/phone.svg" alt="Telefonnummer">Telefon
+                        <img src="<?php bloginfo('template_url')?>/icons/phone.svg" alt="Telefonnummer"><span class="hidden-xs">Telefon</span>
                       </a>
                     </li>
-                    <li>
-                      <a href="<?php bloginfo('blog_url')?>/kontakt">
-                        <img src="<?php bloginfo('template_url')?>/icons/adress.svg" alt="Adresse">Adresse
+                    <li class="icon">
+                      <a href="https://www.google.de/maps/place/W.+Richter+Physiotherapeuten/@52.47333,13.4383124,17z/data=!3m1!4b1!4m5!3m4!1s0x47a84f9f7b3f68e5:0xeabfb16095a4e7dc!8m2!3d52.47333!4d13.4405011" target="_blank">
+                        <img src="<?php bloginfo('template_url')?>/icons/adress.svg" alt="Adresse"><span class="hidden-xs">Adresse</span>
                       </a>
                     </li>
-                    <li>
-                      <a href="<?php bloginfo('blog_url')?>/kontakt">
-                        <img src="<?php bloginfo('template_url')?>/icons/open.svg" alt="Öffnungszeiten">Öffnungszeiten
-                      </a>
-                    </li>
-                    <li>
+                    <li class="icon">
                       <a href="mailto:kontakt@wanja-richter.de">
-                        <img src="<?php bloginfo('template_url')?>/icons/email.svg" alt="Email">E-Mail
+                        <img src="<?php bloginfo('template_url')?>/icons/email.svg" alt="Email"><span class="hidden-xs">E-Mail</span>
                       </a>
                     </li>
-                    <li>
-                      <a class="whatsapp" href="intent://send/4912345678#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end">
+                    <li class="icon">
+                      <a href="<?php bloginfo('blog_url')?>/kontakt">
+                        <img src="<?php bloginfo('template_url')?>/icons/open.svg" alt="Öffnungszeiten"><span class="hidden-xs">Öffnungszeiten</span>
+                        <p class="visible-xs-inline">Mo-Fr 8:00 - 21:00 Uhr | Sa 8:00 - 18:00 Uhr</p>
+                      </a>
+                    </li>
+                    <li class="icon">
+                      <a class="whatsapp hidden-sm hidden-xs" href="intent://send/4912345678#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end">
                         <img src="<?php bloginfo('template_url')?>/icons/whatsapp.svg" alt="WhatsApp">WhatsApp Terminbuchung
                       </a>
                     </li>

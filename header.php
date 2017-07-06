6f90cda31b92cee548f67b044283c96bb384630c
Maximilian Maske <maximilian.maske@gmail.com>
2017-07-07 00:02:53 +0200

    clean up

--- header.php
+++ header.php
@@ -117,36 +117,5 @@
         </div>
         </nav>
         
-        
-        
-        
-        
-<!--
-        <nav class="blog-nav">
-          <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <?php
-            wp_nav_menu( array(
-                'menu'              => 'primary',
-                'theme_location'    => 'primary',
-                'depth'             => 2,
-                'container'         => 'div',
-                'container_class'   => 'collapse navbar-collapse',
-                'container_id'      => 'bs-example-navbar-collapse-1',
-                'menu_class'        => 'nav navbar-nav',
-                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                'walker'            => new WP_Bootstrap_Navwalker())
-            );
-        ?>
-          </div>
-        </nav>
--->
       </div>
     </div>
